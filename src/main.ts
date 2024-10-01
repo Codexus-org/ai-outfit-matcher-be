50fa78e95943d6a9daaae7ca689b9070dbc3d326
faqihfnf <faqih.fnf@gmail.com>
2024-10-02 00:11:36 +0700

    connect to mongodb

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
-import express from "express";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URI as string)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(() => {
+        console.log('Failed to connect to MongoDB');
+    });
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.send("Hello World!");
-})
+app.get('/', (req, res) => {
+    return res.send('Hello World!');
+});
 
-app.listen(8000, () => console.log("Server running on port 8000"));
+app.listen(8000, () => console.log('Server running on port 8000'));
