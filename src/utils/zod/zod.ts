cf0408af75691e66823cfa9e491ee8eb50e49fdb
AndiMahardika <andi.mahardika.mulya@gmail.com>
2024-10-09 09:40:22 -0700

    edit: error zod

--- src/utils/zod/zod.ts
+++ src/utils/zod/zod.ts
@@ -1,16 +1,16 @@
 import { z } from "zod";
 
 const userValidationSchema = z.object({
-    firstName: z.string().min(3, { message: "First name is required" }),
-    lastName: z.string().min(3, { message: "Last name is required" }),
-    username: z.string().min(3, { message: "Username is required" }),
-    email: z.string().email({ message: "Email is required" }),
-    password: z.string().min(8, { message: "Password is required" }),
+    firstName: z.string().min(3, { message: "First name must be at least 3 characters" }),
+    lastName: z.string().min(3, { message: "Last name must be at least 3 characters" }),
+    username: z.string().min(3, { message: "Username must be at least 3 characters" }),
+    email: z.string().email({ message: "Must be a valid email address" }),
+    password: z.string().min(8, { message: "Password must be at least 8 characters" }),
 });
 
 const userLoginSchema = z.object({
-    email: z.string().email({ message: "Email is required" }),
-    password: z.string().min(8, { message: "Password is required" }),
+    email: z.string().email({ message: "Must be a valid email address" }),
+    password: z.string().min(8, { message: "Password must be at least 8 characters" }),
 })
 
 export { userValidationSchema, userLoginSchema }
